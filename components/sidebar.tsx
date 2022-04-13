8440abc77b5726737c455a44295457ac0fd5b4f0
Bishoy <bishoy.samwuel@gmail.com>
2022-04-13 15:42:36 +0200

    Add navigation menu with name, icon, route

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,72 @@
+import NextImage from "next/image";
+import NextLink from "next/link";
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkOverlay,
+  LinkBox,
+} from "@chakra-ui/layout";
+import { MdHome, MdSearch, MdLibraryMusic } from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/Search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/MusicLibrary",
+  },
+];
+
 const Sidebar = () => {
-  return <div> Side Bar Hey </div>
-}
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20">
+          <NextImage src="/logo.png" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+          <Divider paddingY="20px"/>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default Sidebar;
