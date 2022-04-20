d016935c129d1c760db0c54ce9518f7a4ef068a7
Bishoy <bishoy.samwuel@gmail.com>
2022-04-20 12:48:05 +0200

    Create new user with email, password provided Fix syntax error,

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,31 +1,24 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import cookie from 'cookie'
-import { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
 
-export default async (req: NextApiRequest,res: NextApiResponse) => {
-  const salt = bcrypt.genSaltSync()
-  const (email, password) = req.body
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
 
-  let user
+  let user;
 
   try {
     user = await prisma.user.create({
       data: {
-        email, 
-        password: bcrypt.hashSync(Password, salt),
-      }
-    })
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
   } catch (error) {
-  res.status(401)    
-  res.json({error: 'User is already exist'})
-  return
+    res.status(401);
+    res.json({ error: "User is already exist" });
   }
-
-  const token = jwt.sign({
-    email: user.email,
-    id: user.id,
-    time: date.now()
-  }, 'hello', 
-  {'expiresIn': '8h'})
-}
+};
